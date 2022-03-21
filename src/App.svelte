e45086ca5158a7dc2f0105291bb4e35ea5a746e7
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-03-21 12:58:03 +0100

    get params from url pt 1

--- src/App.svelte
+++ src/App.svelte
@@ -26,6 +26,20 @@
 
 	export let name;
 
+	// ROUTING
+	const urlParams = new URLSearchParams(window.location.search);
+	const orderId = urlParams.get('orderId');
+	const transcriptionId = urlParams.get('transcriptionId');
+	const editor = urlParams.has('orderId');
+
+	// const params = new Proxy(new URLSearchParams(window.location.search), {
+	// 	get: (searchParams, prop) => searchParams.get(prop),
+	// });
+	// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+	// let value = params.some_key; // "some_value"
+
+	// https://www.anytranscript.com/nl/editor?orderId={orderId}&transcriptId={transcriptId}
+
 </script>
 
 <style>
@@ -55,19 +69,22 @@
 </style>
 
 {#if isLocaleLoaded}
-<Header />
+	<Header />
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+	<main>
+		<h1>Hello {name}!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</main>
+	<section>
+		<p>orderId: { orderId }</p>
+		<p>transcriptionId: { transcriptionId }</p>
+	</section>
 
-<section>
-
-	<button on:click={changeLanguageToDutch}>Change header to Dutch</button>
-	<button on:click={changeLanguageToEnglish}>Change header to English</button>
-	<button on:click={changeLanguageToDothraki}>Change header to Dothraki</button>
-</section>
+	<section>
+		<button on:click={changeLanguageToDutch}>Change header to Dutch</button>
+		<button on:click={changeLanguageToEnglish}>Change header to English</button>
+		<button on:click={changeLanguageToDothraki}>Change header to Dothraki</button>
+	</section>
 
 {:else}
 	<p>Loading...</p>
